757f81dcf3619db370b2b46cd972037895c52401
Harry <harrywang0619@gmail.com>
2022-05-04 22:25:15 -0400

    fix some

--- parkinsonsneuralnetwork.py
+++ parkinsonsneuralnetwork.py
@@ -14,8 +14,8 @@ def savef(rawdata,rawcategory,hiddenlayer,epoch,filename):
     loflofoutputs, acc, lofj = kfoldcrossvalidneuralnetwork(rawdata,rawcategory,hiddenlayer,k=10,minibatchk=5,lambda_reg=0.1, learning_rate=0.05, epsilon_0=0.00001, softstop=epoch, printq=False)
     accuracyp, precisionp, recallp, fscore_p= meanevaluation(loflofoutputs,1)
     print("Parkinson Data Neural Network with " + str(hiddenlayer) + " hidden layers and " + str(epoch) + " epochs")
-    print("Accuracy:", acc)
-    print("F-score:", fscore_p)
+    print("Accuracy:",  float("{0:.4f}". format(acc)))
+    print("F-score:",  float("{0:.4f}". format(fscore_p)))
     plt.plot(range(epoch+1), lofj[1])
     plt.xlabel("epoch")
     plt.ylabel("J")
@@ -30,8 +30,8 @@ for layer in listoflayers:
     savef(raw_data_p,raw_category_p,layer,epochp_1+1000*n,filenames[n])
     n+=1
 
-# First House Data Neural Network with [4] hidden layers and 5000 epochs
-# Accuracy: 0.855877192982456
-# F-score: 0.9080541501509243
+# Parkinson Data Neural Network with [4] hidden layers and 4000 epochs
+# Accuracy: 0.857(251461988304)
+# F-score: 0.909(2946287120369)
 
 
